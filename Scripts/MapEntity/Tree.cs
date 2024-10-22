031aba6fad1b917a128ad247895f3537f6aa082a
Nico ShiratsuYudachi <shiratsuyudachi@gmail.com>
2024-10-22 22:11:18 +0800

    Add MapEntity Tree

---
+++ Scripts/MapEntity/Tree.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Tree : MapEntity
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+		base._Ready();
+		this.animatedSprite2D.Modulate = Colors.White;
+		this.hardness = 10;
+		this.animatedSprite2D.Play("default");
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+	}
+	
+	public override void ApplyDamage(long amout = 0, Vector2? direction = null, Entity source = null)
+	{
+	}
+}
